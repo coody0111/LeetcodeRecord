4ce7dac3956dabeb3f965f1cbb06719f3c68366c
coody0111 <learncody1@gmail.com>
2024-07-19 00:57:15 -0700

    finished

---
+++ preorder.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        stack<TreeNode *> st;
+        vector<int> result;
+
+        if (root == NULL)
+        {
+            return result;
+        }
+        st.push(root);
+
+        while (!st.empty())
+        {
+            TreeNode *node = st.top(); // get num
+            st.pop();                  // pop num from stack
+            result.push_back(node->val);
+            if (node->right)
+                st.push(node->right);
+            if (node->left)
+                st.push(node->left);
+        }
+        return result;
+    }
+};
