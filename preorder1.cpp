23949d5b81ed8ffc5564fae763bc2cdaffa54d2c
coody0111 <learncody1@gmail.com>
2024-07-20 02:47:33 -0700

    add some folder

---
+++ preorder1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+    }
+};
