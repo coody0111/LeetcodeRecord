37902fcffd1a6b1f3dae5466bd6752605549470c
Cody Liu <codyliu@CodydeMacBook-Pro.local>
2024-08-13 17:37:56 +0800

    more focus on bit operation

---
+++ bit_manipulation/137/137_Single_number_II.c
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int singleNumber(vector<int> &nums)
+    {
+        int ans = 0;
+        for (int i = 0; i <= 31; ++i)
+        {
+            int cnt = 0;
+            for (auto &num : nums)
+            {
+                cnt += (num >> i) & 1;
+            }
+            cnt %= 3;
+            ans |= (cnt << i);
+        }
+        return ans;
+    }
+};
---
+++ bit_manipulation/645/645_Set Mismatch.c
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+    vector<int> findErrorNums(vector<int> &nums)
+    {
+        int n = nums.size();
+        int eor = 0;
+        for (int i = 1; i <= n; ++i)
+        {
+            eor ^= i ^ nums[i - 1];
+        }
+        int lowbit = eor & (-eor);
+        int a = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (i & lowbit)
+            {
+                a ^= i;
+            }
+            if (nums[i - 1] & lowbit)
+            {
+                a ^= nums[i - 1];
+            }
+        }
+        int b = eor ^ a;
+        for (int i = 0; i < n; ++i)
+        {
+            if (nums[i] == a)
+            {
+                return {a, b};
+            }
+        }
+        return {b, a};
+    }
+};
